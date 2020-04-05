e5d1fb95ff9795d3b79f56d28f1aec18ab2257ee
RafaelGoulartB <rafagoulartb@gmail.com>
2020-04-05 08:37:04 -0300

    Some fixes

--- js/nav-scoll.js
+++ js/nav-scoll.js
@@ -1,8 +1,11 @@
 const menuItems = document.querySelectorAll('.link a')
+const mobileMenu = document.querySelector('.menu-items')
 
 menuItems.forEach((item) => {
   item.addEventListener('click', event => {
     event.preventDefault()
+
+    closeMobileMenu()
 
     const element = event.target
     const id = element.getAttribute('href')
@@ -14,3 +17,11 @@ menuItems.forEach((item) => {
     })
   })
 })
+
+function closeMobileMenu() {
+  toogleButton.classList.remove('fa-times')
+  toogleButton.classList.add('fa-bars')
+  
+  menu.classList.remove('menu-items-active')
+  menu.classList.add('menu-items-disable')
+}

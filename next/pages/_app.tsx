9c76b7aa94d2514956cb67676a7ae9f05ec92dfe
RafaelGoulartB <rafagoulartb@gmail.com>
2020-09-20 08:05:24 -0300

    📦 feat: init next.js project with typescript

---
+++ next/pages/_app.tsx
@@ -0,0 +1,8 @@
+import '../styles/globals.css'
+import { AppProps } from 'next/app'
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ next/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Page from '../components/page'
+
+const Home: React.FC = () => {
+  return (
+    <Page>
+      <></>
+    </Page>
+  )
+}
+
+export default Home

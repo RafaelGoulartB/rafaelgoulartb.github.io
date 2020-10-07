5ec9708ed7d1e0bc92ca9d85adc4d25bb477dc29
RafaelGoulartB <rafagoulartb@gmail.com>
2020-10-07 09:22:11 -0300

    📦 refact: remove unecessary imports

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Flex, Heading, Box, Link, Text, Divider } from '@chakra-ui/core'
+import { Flex, Box, Link, Divider } from '@chakra-ui/core'
 import Logo from './logo'
 
 interface MenuItem {

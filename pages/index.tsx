361d424fbb2f4684df83c4cb0767f51e3ebd3933
RafaelGoulartB <rafagoulartb@gmail.com>
2020-10-08 18:13:53 -0300

    📦 feat: change the bg color to #fafafa

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,15 @@ import { Grid } from '@chakra-ui/core'
 import Header from '../components/header'
 import About from '../components/about'
 import Portfolio from '../components/portfolio'
+import Services from '../components/services'
 
 const Home: React.FC = () => {
   return (
-    <Grid>
+    <Grid bg="#fafafa">
       <Header />
       <About />
       <Portfolio />
+      <Services />
     </Grid>
   )
 }

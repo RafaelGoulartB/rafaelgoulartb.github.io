dbe2bf2f3f81d1aaa89c48e48c8742429dff0758
RafaelGoulartB <rafagoulartb@gmail.com>
2020-10-09 08:27:48 -0300

    📦 feat: add tooltip at projects links

--- components/portfolio/item.tsx
+++ components/portfolio/item.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, Text, Image, Flex, Button, Heading } from '@chakra-ui/core'
+import { Link, Text, Image, Flex, Button, Heading, Tooltip } from '@chakra-ui/core'
 import { FaGithub, FaGooglePlay, FaLink } from 'react-icons/fa'
 import { ProjectsInterface } from '../../texts/projects'
 
@@ -43,43 +43,49 @@ const Item: React.FC<ProjectsInterface> = ({
         <Flex flexDir="row">
           {playStoreUrl && (
             <Link isExternal href={playStoreUrl} mr="4">
-              <Button
-                fontWeight="normal"
-                leftIcon={FaGooglePlay}
-                bg="blue.500"
-                variantColor="black"
-                color="white"
-              >
-                Play Store
-              </Button>
+              <Tooltip hasArrow aria-label="See at Play Store" label="See at Play Store" placement="top" bg="blue.500">
+                <Button
+                  fontWeight="normal"
+                  leftIcon={FaGooglePlay}
+                  bg="blue.500"
+                  variantColor="black"
+                  color="white"
+                >
+                  Play Store
+                </Button>
+              </Tooltip>
             </Link>
           )}
 
           {websiteUrl && (
             <Link isExternal href={websiteUrl} mr="4">
-              <Button
-                fontWeight="normal"
-                leftIcon={FaLink}
-                bg="blue.500"
-                variantColor="black"
-                color="white"
-              >
-                Website
-              </Button>
+              <Tooltip hasArrow aria-label="See Website Online" label="See Website Online" placement="top" bg="blue.500">
+                <Button
+                  fontWeight="normal"
+                  leftIcon={FaLink}
+                  bg="blue.500"
+                  variantColor="black"
+                  color="white"
+                >
+                  Website
+                </Button>
+              </Tooltip>
             </Link>
           )}
 
           {githubUrl && (
             <Link isExternal href={githubUrl} mr="4">
-              <Button
-                fontWeight="normal"
-                leftIcon={FaGithub}
-                bg="gray.500"
-                variantColor="black"
-                color="white"
-              >
-                Github
-              </Button>
+              <Tooltip hasArrow aria-label="Github Repository" label="Github Repository" placement="top" bg="gray.500">
+                <Button
+                  fontWeight="normal"
+                  leftIcon={FaGithub}
+                  bg="gray.500"
+                  variantColor="black"
+                  color="white"
+                >
+                  Github
+                </Button>
+              </Tooltip>
             </Link>
           )}
         </Flex>

8e131a1aec4b561786b4f2286ab5747c494142f5
RafaelGoulartB <rafagoulartb@gmail.com>
2020-10-22 08:17:49 -0300

    refact: add sitemap and robots.txt for seo (#11)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,18 +7,8 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500&display=swap"
-          rel="stylesheet"
-        />
-
         <title>Rafael Goulart - FullStack Developer Freelancer</title>
-
-        <meta
-          name="description"
-          content="I'm Rafael Goulart, Full Stack Developer and I work with React.js, Next.js, React Native and Node.js, I’m currently working on freelance projects."
-        />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <ThemeContainer>
         <Component {...pageProps} />

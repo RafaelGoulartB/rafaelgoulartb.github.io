9fc92e1804fb0420030188e7d32117357a6dd7de
RafaelGoulart <rafagoulartb@gmail.com>
2020-10-30 07:46:54 -0300

    update: change description in openGraph

--- config/seo.ts
+++ config/seo.ts
@@ -5,7 +5,7 @@ const seoConfig = {
   openGraph: {
     url: "https://rafaelgoulartb.vercel.app/",
     title: "Rafael Goulart - Frontend Developer",
-    description: "I'm Rafael Goulart, Freelancer Frontend Developer and I work with React.js, Next.js, React Native and Node.js.",
+    description: "Portfolio with all my projects as a Frontend developer.",
     site_name: "Rafael Goulart - Frontend Developer",
   },
 };

dd2041afcbbb4d1bdd78ea0a086fcf32c97b1de1
RafaelGoulart <rafagoulartb@gmail.com>
2020-10-30 07:37:38 -0300

    update: change the title of site

--- config/seo.ts
+++ config/seo.ts
@@ -1,12 +1,12 @@
 
 const seoConfig = {
-  title: "Rafael Goulart - FullStack Developer Freelancer",
-  description: "I'm Rafael Goulart, Freelancer Full Stack Developer and I work with React.js, Next.js, React Native and Node.js.",
+  title: "Rafael Goulart - Frontend Developer Freelancer",
+  description: "I'm Rafael Goulart, Freelancer Frontend Developer and I work with React.js, Next.js, React Native and Node.js.",
   openGraph: {
     url: "https://rafaelgoulartb.vercel.app/",
-    title: "Rafael Goulart - Freelancer",
-    description: "I'm Rafael Goulart, Freelancer Full Stack Developer and I work with React.js, Next.js, React Native and Node.js.",
-    site_name: "Rafael Goulart - Freelancer",
+    title: "Rafael Goulart - Frontend Developer",
+    description: "I'm Rafael Goulart, Freelancer Frontend Developer and I work with React.js, Next.js, React Native and Node.js.",
+    site_name: "Rafael Goulart - Frontend Developer",
   },
 };
 

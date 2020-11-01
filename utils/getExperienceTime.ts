3f317b55de1a41a3d808df06c5bbe28d27e6e96a
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-01 14:18:39 -0300

    feat: add getExperience util (#21)

---
+++ utils/getExperienceTime.ts
@@ -0,0 +1,20 @@
+const getExperienceTime = (startDate: Date) => {
+  const currDate = new Date();
+
+  var months;
+  months = (currDate.getFullYear() - startDate.getFullYear()) * 12;
+  months -= startDate.getMonth();
+  months += currDate.getMonth();
+
+  if (months < 12) {
+    return `${months} month${months > 1 ? 's' : ''} experience`
+  }
+  else {
+    const restMonths = months % 12
+    const years = Math.round(months / 12)
+
+    return `${years} year${years > 1 ? 's' : ''} experience`
+  }
+}
+
+export default getExperienceTime

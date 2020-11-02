92063fcf0eac57af8821e1d2c237d599ff68d7e6
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-02 08:54:31 -0300

    feat: add font preload

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,24 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link
+            rel="preload"
+            href="/fonts/poppins-v13-latin-300.woff2"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/poppins-v13-latin-regular.woff2"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/poppins-v13-latin-500.woff2"
+            as="font"
+            crossOrigin=""
+          />
           <meta title="Rafael Goulart - FullStack Developer Freelancer" />
           <link rel="manifest" href="/manifest.json" />
           <meta name="theme-color" content="#fafafa" />

749a454ddb646f1b452e421d50cdcbaba593e842
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-04 09:28:52 -0300

    feat: create constants to nav links

---
+++ components/header/constants.ts
@@ -0,0 +1,23 @@
+export interface navLinksProps {
+  label: string
+  link: string
+}
+
+export const navLinks: navLinksProps[] = [
+  {
+    label: 'About',
+    link: '#about'
+  },
+  {
+    label: 'Portfolio',
+    link: '#portfolio'
+  },
+  {
+    label: 'Services',
+    link: '#services'
+  },
+  {
+    label: 'Skills',
+    link: '#skills'
+  },
+]

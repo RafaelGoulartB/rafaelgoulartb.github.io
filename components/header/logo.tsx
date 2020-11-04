7fbd260992e0d80c27390c3dd8a7837358dadf52
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-04 10:06:31 -0300

    feat: add 'back to home' link in logo

--- components/header/logo.tsx
+++ components/header/logo.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Heading, Text } from '@chakra-ui/core'
+import { Heading, Link, Text } from '@chakra-ui/core'
 
 const Logo: React.FC = () => {
   return (
-    <Heading as="h1" fontSize="2xl" color="white">
-      <Text display="inline" fontWeight="bold">
-        Rafael Goulart
-      </Text>
-    </Heading>
+    <Link href="#" _hover={{ textDecor: 'none' }}>
+      <Heading as="h1" fontSize="2xl" color="white">
+        <Text display="inline" fontWeight="bold">
+          Rafael Goulart
+        </Text>
+      </Heading>
+    </Link>
   )
 }
 

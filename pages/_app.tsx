61799d26074fcabac5aebabdcc7f1a49f3e2c1a4
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-05 13:10:25 -0300

    update: remove ga tag cause it's not working

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,10 @@ import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { DefaultSeo } from 'next-seo'
 import ThemeContainer from '../contexts/theme/theme-container'
-import Router from 'next/router'
 
 import '../styles/global.css'
 
 import seoConfig from '../config/seo'
-import * as gtag from '../utils/gtag'
-
-Router.events.on('routeChangeComplete', url => gtag.pageview(url))
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (

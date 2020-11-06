9825351acd2815de99ebd36fc18be3dad6c8f7ae
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-06 13:06:25 -0300

    refact: section title to use heading props

--- components/section-title.tsx
+++ components/section-title.tsx
@@ -1,12 +1,8 @@
 import React from 'react'
 
-import { Heading } from '@chakra-ui/core'
+import { Heading, HeadingProps } from '@chakra-ui/core'
 
-interface SectionTitle {
-  title: string
-}
-
-const SectionTitle: React.FC = ({ children }) => {
+const SectionTitle: React.FC<HeadingProps> = ({ children, ...props }) => {
   return (
     <Heading
       as="h4"
@@ -14,6 +10,7 @@ const SectionTitle: React.FC = ({ children }) => {
       fontSize="6xl"
       fontWeight="bold"
       mb="12"
+      {...props}
     >
       {children}
     </Heading>

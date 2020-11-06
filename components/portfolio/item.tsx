2336b02a9b8a4d4f6cfe82168068109fc9652967
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-06 13:38:11 -0300

    refact: add project-buttons to item section

--- components/portfolio/item.tsx
+++ components/portfolio/item.tsx
@@ -4,15 +4,13 @@ import {
   Text,
   Box,
   Flex,
-  Button,
   Heading,
-  Tooltip,
   Badge,
   PseudoBox
 } from '@chakra-ui/core'
 import Image from 'next/image'
-import { FaGithub, FaGooglePlay, FaLink } from 'react-icons/fa'
 import { ProjectsInterface } from '../../data/types'
+import ProjectButtons from './project-buttons'
 
 const Item: React.FC<ProjectsInterface> = ({
   name,
@@ -81,80 +79,11 @@ const Item: React.FC<ProjectsInterface> = ({
           )}
         </Flex>
 
-        {/* Buttons */}
-        <Flex flexDir="row" mt="6">
-          {playStoreUrl && (
-            <Link isExternal href={playStoreUrl} mr="4">
-              <Tooltip
-                hasArrow
-                aria-label="See at Play Store"
-                label="See at Play Store"
-                placement="top"
-                bg="blue.500"
-              >
-                <Button
-                  fontWeight="normal"
-                  leftIcon={FaGooglePlay}
-                  bg="blue.500"
-                  variantColor="black"
-                  color="white"
-                  _hover={{
-                    textDecoration: 'underline'
-                  }}
-                >
-                  Play Store
-                </Button>
-              </Tooltip>
-            </Link>
-          )}
-
-          {websiteUrl && (
-            <Link isExternal href={websiteUrl} mr="4">
-              <Tooltip
-                hasArrow
-                aria-label="See Website Online"
-                label="See Website Online"
-                placement="top"
-                bg="blue.500"
-              >
-                <Button
-                  fontWeight="normal"
-                  leftIcon={FaLink}
-                  bg="blue.500"
-                  variantColor="black"
-                  color="white"
-                  _hover={{
-                    textDecoration: 'underline'
-                  }}
-                >
-                  Website
-                </Button>
-              </Tooltip>
-            </Link>
-          )}
-
-          {githubUrl && (
-            <Link isExternal href={githubUrl} mr="4">
-              <Tooltip
-                hasArrow
-                aria-label="Github Repository"
-                label="Github Repository"
-                placement="top"
-                bg="gray.500"
-              >
-                <Button
-                  fontWeight="normal"
-                  leftIcon={FaGithub}
-                  bg="gray.500"
-                  variantColor="black"
-                  color="white"
-                >
-                  Github
-                </Button>
-              </Tooltip>
-            </Link>
-          )}
-        </Flex>
+        <ProjectButtons
+          websiteUrl={websiteUrl}
+          playStoreUrl={playStoreUrl}
+          githubUrl={githubUrl}
+        />
       </Flex>
     </PseudoBox>
   )

cb38f6105f41cdc0bde6c65c13dadc3dd43bba87
RafaelGoulartB <rafagoulartb@gmail.com>
2020-11-30 07:48:25 -0300

    feat: add open graph seo config

--- config/seo.ts
+++ config/seo.ts
@@ -1,13 +1,26 @@
 
 const seoConfig = {
-  title: "Rafael Goulart - Frontend Developer Freelancer",
+  title: "Rafael Goulart - Frontend Developer Portfolio",
   description: "I'm Rafael Goulart, Freelancer Frontend Developer and I work with React.js, Next.js, React Native and Node.js.",
   openGraph: {
-    url: "https://rafaelgoulartb.vercel.app/",
-    title: "Rafael Goulart - Frontend Developer",
-    description: "Portfolio with all my projects as a Frontend developer.",
-    site_name: "Rafael Goulart - Frontend Developer",
+    url: 'https://rafaelgoulartb.vercel.app/',
+    title: 'Open Graph Title',
+    description: 'Open Graph Description',
+    images: [
+      {
+        url: '/og/og-image.png',
+        width: 900,
+        height: 800,
+        alt: 'Rafael Goulart Portfolio - Next.js, Typescript and React Native',
+      }
+    ],
+    site_name: 'Rafael Goulart - Frontend Developer Portfolio',
   },
+  twitter: {
+    handle: '@handle',
+    site: 'https://rafaelgoulartb.vercel.app/',
+    cardType: 'summary_large_image',
+  }
 };
 
 export default seoConfig;

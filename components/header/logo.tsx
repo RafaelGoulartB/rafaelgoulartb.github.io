ecd20521327a40c88920ddf0038789c6f4ed386a
RafaelGoulartB <rafagoulartb@gmail.com>
2020-12-14 09:28:42 -0300

    style: update logo color scheme

--- components/header/logo.tsx
+++ components/header/logo.tsx
@@ -4,7 +4,7 @@ import { Heading, Link, Text } from '@chakra-ui/react'
 const Logo: React.FC = () => {
   return (
     <Link href="#" _hover={{ textDecor: 'none' }}>
-      <Heading as="h1" fontSize="2xl" color="white">
+      <Heading as="h1" fontSize="2xl" color="gray.500">
         <Text display="inline" fontWeight="bold">
           Rafael Goulart
         </Text>
